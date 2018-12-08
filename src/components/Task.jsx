205b78df46312eccdf354b5361bf503e591f6333
Sergio Garibay <sergio.pascual@thinker.mx>
2018-12-07 19:07:21 -0600

    Fontawesome añadido

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Task = (props) => (
-  <li>
-    <h3>{props.task}</h3>
-    {/*<span>Status 0/1</span>*/}
-  </li>
+  <div className="taskItem">
+    <li>
+      <h3>
+      <a><i class="fas fa-trash-alt"></i></a>
+      {props.task} </h3>
+      {/*<span>Status 0/1</span>*/}
+    </li>
+  </div>
 )
 
 export default Task;
